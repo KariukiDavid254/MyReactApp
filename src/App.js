136ade58a1a8d8d0ae0f287d8f81054c79d7a724
KariukiDavid254 <kariukidavidwangari254@gmail.com>
2023-04-17 11:49:11 +0300

    My First App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+     <h1> First App</h1>
+     <img src="https://images.unsplash.com/photo-1596639410348-8470f7fa9f84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80"alt="" srcSet=""></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
